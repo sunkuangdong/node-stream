a120b57e6d4324a7ba4a8eaac0c26f100b8f8115
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-07-25 14:22:10 +0800

    Update 1.js

--- src/1.js
+++ src/1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const stream = fs.createWriteStream('./big_file.txt');
+const stream = fs.createWriteStream('./../big_file.txt');
 
 for (let i = 0; i < 1000000; i++) {
   stream.write(`这是第${i}行内容\n`);
